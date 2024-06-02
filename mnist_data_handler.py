eeecc96e05abdff750c4e6623062fea218c9c69a
Lucas Bruckbauer <lucas.bruckbauer@gmail.com>
2024-06-02 22:33:14 +0100

    Initial Commit

---
+++ mnist_data_handler.py
@@ -0,0 +1,100 @@
+import numpy as np
+import struct
+from array import array
+from os.path import join
+
+import random
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+class MnistDataHandler: 
+    def __init__(self):
+
+        self.current_directory = Path(__file__).resolve().parent
+        self.training_images_filepath = join(self.current_directory, 'mnist/train-images-idx3-ubyte/train-images-idx3-ubyte')
+        self.training_labels_filepath = join(self.current_directory, 'mnist/train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+        self.test_images_filepath = join(self.current_directory, 'mnist/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+        self.test_labels_filepath = join(self.current_directory, 'mnist/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+
+    
+    def read_images_labels(self, images_filepath, labels_filepath):        
+        labels = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            labels = array("B", file.read())        
+        
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())        
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            images[i][:] = img            
+        
+        return images, np.array(labels)
+    
+    def load_training_data(self):
+        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        x_train = np.reshape(x_train, (-1, 28*28))
+        
+        return x_train, y_train
+    
+    def load_test_data(self):
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        x_test = np.reshape(x_test, (-1, 28*28))
+
+        return x_test, y_test
+    
+    def plot_images(self, images, title_texts):
+
+        images = np.reshape(images, (np.shape(images)[0], 28, 28))
+
+        cols = 5
+        rows = int(len(images)/cols) + 1 
+
+        images_plot = plt.figure(figsize=(5,5))
+        images_plot.subplots_adjust(hspace = 1)
+
+        index = 1    
+        for i in range(len(images)):        
+            image = images[i]        
+            title_text = title_texts[i]
+            image_subplot = plt.subplot(rows, cols, index)
+            image_subplot.axis('off')
+            image_subplot.imshow(image, cmap=plt.cm.gray)
+            
+            if (title_text != ''):
+                image_subplot.set_title(title_text, fontsize = 15)
+            
+            images_plot.add_subplot(image_subplot)
+            index += 1
+        
+        return images_plot
+    
+    def generate_random_training_samples(self, sample_count, x_train, y_train):
+        
+        images_to_show = []
+        titles_to_show = []
+        for i in range(0, sample_count):
+            r = random.randint(1, 60000)
+            images_to_show.append(x_train[r])
+            titles_to_show.append('y = ' + str(y_train[r]))
+
+        return images_to_show, titles_to_show    
+
+def main(): 
+    mnist_data_handler = MnistDataHandler()
+    x_train, y_train = mnist_data_handler.load_training_data()
+    images_to_show, titles_to_show = mnist_data_handler.generate_random_training_samples(20, x_train, y_train)
+    mnist_data_handler.plot_images(images_to_show, titles_to_show)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ neural_network.py
@@ -0,0 +1,114 @@
+import numpy as np
+from mnist_data_handler import MnistDataHandler
+
+class NeuralNetwork:
+    def __init__(self, layers):
+
+        self.layers = layers
+        
+    def init_weights_and_biases(self, X, layers):
+        W1 = np.random.randn(layers[0], X.shape[0]) * np.sqrt(2.0/X.shape[0])
+        W2 = np.random.randn(layers[1], layers[0]) * np.sqrt(2.0/layers[0])
+        W3 = np.random.randn(layers[2], layers[1] ) * np.sqrt(2.0/layers[1])
+
+        b1 = np.random.randn(layers[0], 1) * 0.1
+        b2 = np.random.randn(layers[1], 1) * 0.1
+        b3 = np.random.randn(layers[2], 1) * 0.1
+
+        return W1, b1, W2, b2, W3, b3
+
+
+    def relu(self, Z):
+        return np.maximum(0, Z)
+    
+    def relu_derivative(self, Z):
+        return Z > 0
+
+    def softmax(self, Z):
+        Z -= np.max(Z, axis=0)  # Subtract max value for numerical stability
+        A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
+        return A
+    
+    def one_hot(self, Y):
+        return np.array([i == Y for i in range(10)])
+    
+    def categorical_cross_entropy(self, Y, Y_hat):
+        # Avoid numerical instability by clipping values
+        Y_hat = np.clip(Y_hat, 1e-15, 1 - 1e-15)
+        return -np.mean(np.sum(Y * np.log(Y_hat), axis=1))
+
+    def predictions(self, A2):
+        return np.argmax(A2, axis=0)
+
+    def accuracy(self, Y, Y_hat):
+        return np.mean(Y == Y_hat)
+
+    def dense(self, a_in, W, b, activation):
+        z = W.dot(a_in) + b
+        if(activation == 'relu'):   
+            a_out = self.relu(z)
+        elif(activation == 'softmax'):
+            a_out = self.softmax(z)
+        return z, a_out
+
+    def forward_prop(self, X, W1, b1, W2, b2, W3, b3):
+        z1, a1 = self.dense(X, W1, b1, 'relu')
+        z2, a2 = self.dense(a1, W2, b2, 'relu')
+        z3, a3 = self.dense(a2, W3, b3, 'softmax')
+        
+        return a1, a2, a3, z1, z2, z3
+    
+    def back_prop(self, Z1, A1, Z2, A2, W2, A3, W3, X, Y):
+        m = Y.size
+
+        dZ3 = A3 - self.one_hot(Y)
+        dW3 = 1/m * dZ3.dot(A2.T)
+        db3 = 1/m * np.sum(dZ3, 1).reshape(-1, 1)
+
+        dZ2 = W3.T.dot(dZ3) * self.relu_derivative(Z2)
+        dW2 = 1/m * dZ2.dot(A1.T)
+        db2 = 1/m * np.sum(dZ2, 1).reshape(-1, 1)
+
+        dZ1 = W2.T.dot(dZ2) * self.relu_derivative(Z1)
+        dW1 = 1/m * dZ1.dot(X.T)
+        db1 = 1/m * np.sum(dZ1, 1).reshape(-1, 1)
+
+        return dW1, db1, dW2, db2, dW3, db3
+
+    def update_params(self, W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
+
+        W1 -= alpha * dW1
+        b1 -= alpha * db1
+        W2 -= alpha * dW2
+        b2 -= alpha * db2
+        W3 -= alpha * dW3
+        b3 -= alpha * db3
+
+        return W1, b1, W2, b2, W3, b3
+            
+    def training_loop(self, X, Y, epochs, alpha):
+        W1, b1, W2, b2, W3, b3 = self.init_weights_and_biases(X, self.layers)
+        for i in range(epochs):
+            a1, a2, a3, z1, z2, z3 = self.forward_prop(X, W1, b1, W2, b2, W3, b3)
+            dW1, db1, dW2, db2, dW3, db3 = self.back_prop(z1, a1, z2, a2, W2, a3, W3, X, Y)
+            W1, b1, W2, b2, W3, b3 = self.update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha)
+            if(i % 10 == 0):
+                print('Epoch: ', i)
+                print('Accuracy: ', self.accuracy(Y, self.predictions(a3)))
+                print('Loss: ', self.categorical_cross_entropy(self.one_hot(Y), a3))
+        print('Accuracy after training: ', self.accuracy(Y, self.predictions(a3)))
+        
+
+def main():
+    mnist_data_handler = MnistDataHandler()
+    x_train, y_train = mnist_data_handler.load_training_data()
+
+    x_train = x_train.T
+    x_train = x_train / 255
+
+    neural_network = NeuralNetwork([40, 20, 10])
+    neural_network.training_loop(x_train, y_train, 150, 0.1)
+
+    
+if __name__ == '__main__':
+    main()
